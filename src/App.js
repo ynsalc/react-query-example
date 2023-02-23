1acef9fb34dc26a8ccd4854a3f2908d91e738b7a
Yunus Alıcı <yunusac51@gmail.com>
2023-02-23 18:10:48 +0300

    react query added and example app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
+  const getPosts = () => {
+    return axios
+      .get("https://jsonplaceholder.typicode.com/posts", {
+        params: { _sort: "title" },
+      })
+      .then((res) => res.data);
+  };
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => getPosts(),
+  });
   return (
     <div>
-      <h1 className="text-3xl font-bold text-green-300">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-green-400">Those is Post Titles</h1>
+      {data &&
+        data.map((item) => (
+          <p className="text-red-300 text-xl" key={item.id}>{item.title}</p>
+        ))}
     </div>
   );
 }
